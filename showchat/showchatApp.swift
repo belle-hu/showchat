c8bab8449d1742d1b7019cdc5aa92e9673364b5a
belle hu <bh552@cornell.edu>
2024-03-29 11:59:16 -0400

    Initialize repo

---
+++ showchat/showchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  showchatApp.swift
+//  showchat
+//
+//  Created by Belle Hu on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct showchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
